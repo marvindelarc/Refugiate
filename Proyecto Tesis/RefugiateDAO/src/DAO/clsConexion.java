84bf47af6d8e1becb25c9b8f8f73a006ff1d98a7
marvindelarc <marvindelarc@gmail.com>
2015-03-31 22:54:15 -0500

    update

---
+++ Proyecto Tesis/RefugiateDAO/src/DAO/clsConexion.java
@@ -0,0 +1,31 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class clsConexion {
+    public static String driver = "com.mysql.jdbc.Driver";
+    public static String url = "jdbc:mysql://204.93.216.11:3306/servitec1_refugiate";
+    public static String usuario = "servitec1_admin";
+    public static String clave = "@123Soltrux123";
+//    public static String url = "jdbc:mysql://localhost:3306/servitec1_refugiate";
+//    public static String usuario = "root";
+//    public static String clave = "123456";
+    
+    public clsConexion() {
+    }
+    public static Connection getConnection() throws Exception{
+        Connection cnn = null;
+        try {
+             Class.forName(driver);
+            cnn=DriverManager.getConnection(url,usuario,clave);
+            System.out.println("conexion establecida");
+        } catch (Exception e) {
+             System.out.println("no se pùdo establecer la conexion");
+            System.exit(0);
+        }
+             return cnn;
+    }
+
+}
